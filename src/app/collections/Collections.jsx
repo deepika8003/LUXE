72709f6fb2dc98699b9be62106e14b560c74bfb9
DEEPIKA <deepikadb8003@gmail.com>
2026-03-10 12:22:19 +0530

    collection page

---
+++ src/app/collections/Collections.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Collections = () => {
+  return (
+    <div className="mt-20 py-20">
+      <h1>collection page </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+        nam?
+      </p>
+    </div>
+  );
+};
+
+export default Collections;
